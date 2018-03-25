4fe96814afce5e6068ae908b5b2a383365c880fb
viktorWho <v.konkevich@gmail.com>
2018-03-25 05:48:42 +0200

    Initial Commit

---
+++ pushingToGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pushingToGithub
+//
+//  Created by Viktor K on 25.03.2018.
+//  Copyright © 2018 viktor.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
